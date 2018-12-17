b78d3f55f4479eaf9fd55e1017b41d07e18d4e20
CuiZhenKai <626512587@qq.com>
2018-12-17 14:17:06 +0800

    '项目初次提交'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>我是首页</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
+
+
+<style lang="less" scoped>
+
+</style>
